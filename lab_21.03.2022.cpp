957829cc36ebe35e8437035a2c543cad6ebf66b6
Bartosz Małyjasiak <bartosz.malyjasiak@gmail.com>
2022-03-28 13:01:33 +0200

    hybrydowy constructor problem

--- lab_21.03.2022.cpp
+++ lab_21.03.2022.cpp
@@ -16,6 +16,7 @@ public:
         moc = m;
         przyspieszenie = p;
         zasieg = z;
+        pokaz();
     }
 
     void pokaz() {
@@ -34,12 +35,12 @@ class Elektryczny : virtual public Samochod {
 
 public:
 
-    Elektryczny(int sb = 0, string r = "kombi", string ma = "Volkswagen", string mo = "Passat", int ro = 2003, int m = 140, int p = 5, int z = 900) :Samochod(r, ma, mo, ro, m, p, z) {
+    Elektryczny(int sb = 0, string r = "hatchback", string ma = "Volkswagen", string mo = "Passat", int ro = 2003, int m = 140, int p = 5, int z = 900) :Samochod(r, ma, mo, ro, m, p, z) {
         stan_baterii = sb;
+        pokaz();
     }
 
     void pokaz() {
-        Samochod::pokaz();
         cout << "Stan baterii: " << stan_baterii << endl;
     }
 };
@@ -49,34 +50,37 @@ class Spalinowy : virtual public Samochod {
 
 public:
 
-    Spalinowy( string r = "kombi", string ma = "Volkswagen", string mo = "Passat", int ro = 2003, int m = 140, int p = 5, int z = 900, string typ_p = "benzyna") :Samochod(r, ma, mo, ro, m, p, z) {
+    Spalinowy( string r = "sedan", string ma = "Volkswagen", string mo = "Passat", int ro = 2003, int m = 140, int p = 5, int z = 900, string typ_p = "diesel") :Samochod(r, ma, mo, ro, m, p, z) {
         typ_paliwa = typ_p;
+        pokaz();
     }
 
     void pokaz() {
-        Samochod::pokaz();
         cout << "Rodzaj paliwa: " << typ_paliwa << endl;
     }
 };
 
 class Hybrydowy :public Spalinowy, public Elektryczny {
 public:
-    Hybrydowy(int sb = 0, string r = "kombi", string ma = "Volkswagen", string mo = "Passat", int ro = 2003, int m = 140, int p = 5, int z = 900, string typ_p ="benzyna", int stan_b=50) :Elektryczny(stan_b), Spalinowy(typ_p), Samochod(r, ma, mo, ro, m, p, z) {
-
+    Hybrydowy(string r = "suv", string ma = "Volkswagen", string mo = "Passat", int ro = 2003, int m = 140, int p = 5, int z = 900, string typ_p ="benzyna", int sb=60) :Elektryczny(sb), Spalinowy(typ_p), Samochod(r, ma, mo, ro, m, p, z) {
+  
     }
 
     void pokaz() {
-        
-        
+        Samochod::pokaz();
+        Spalinowy::pokaz();
+        Elektryczny::pokaz();
     }
 };
 
 int main()
 {
-    Elektryczny samochod_elektryczny;
+    //Elektryczny samochod_elektryczny;
     Hybrydowy samochod_hybrydowy;
-    samochod_hybrydowy.pokaz();
+    //samochod_hybrydowy.pokaz();
     //samochod_elektryczny.pokaz();
+    //cout << endl;
+   // samochod_hybrydowy.pokaz();
 
     return 0;
 }
